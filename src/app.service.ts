e4baeb259b0058bc2227a4ace2c148fc148ad59b
Bryan Oliveira <bsantos.cpp@gmail.com>
2024-01-20 10:37:16 -0300

    created users resource

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUsers(): Array<any> {
+    return [{
+      name: 'Bryan',
+      email: 'bso.compras@gmail.com'
+    }]
   }
 }

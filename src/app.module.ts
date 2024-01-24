7da9aecb6d7f754781196131f0e1ca421a9beb64
Bryan Oliveira <bsantos.cpp@gmail.com>
2024-01-24 11:47:44 -0300

    created auth resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PaymentMethodModule } from './payment_method/payment_method.module';
 import { SalesModule } from './sales/sales.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, PaymentMethodModule, SalesModule],
+  imports: [UsersModule, PrismaModule, PaymentMethodModule, SalesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

0088ea68465367d4bea9d51cfc0e7d0cb136dd5b
Bryan Oliveira <bsantos.cpp@gmail.com>
2024-01-24 11:47:29 -0300

    installed class-validator for dto fields

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,22 @@
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
+
 export class CreateUserDto {
+
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsString()
+    @IsNotEmpty()
     document: string;
+
+    @IsEmail()
+    @IsString()
+    @IsNotEmpty()
     email: string;
+
+    @IsStrongPassword()
+    @IsString()
+    @IsNotEmpty()
     password: string;
 }
